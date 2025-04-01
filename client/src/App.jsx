7cd4dbb6215d6c6d84f7b211963671269553f2b3
wuttichai-jeenkaew <wuttichai.jeenkaew@gmail.com>
2025-04-01 13:33:13 +0700

    Assignment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+import { createContext } from "react";
+
+
+export const userDataContext = createContext({})
 
 function App() {
+
+  
   const userData = {
     username: "John",
     avatar: "https://placedog.net/100/100",
@@ -13,12 +18,14 @@ function App() {
 
   return (
     <div className="App">
+      <userDataContext.Provider value={userData}>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/view/:id" element={<ViewProductPage />} />
         </Routes>
       </Router>
+      </userDataContext.Provider>
     </div>
   );
 }
